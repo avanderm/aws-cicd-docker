5e37a138e208c2880663bab6ed819d66bcd0d368
Antoine Vandermeersch <avdmeers@gmail.com>
2020-06-04 11:02:43 +0200

    ECS logging tests

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     print("Starting consumer")
 
     while True:
-        for message in queue.receive_messages(WaitTimeSeconds=10):
+        for message in queue.receive_messages():
             try:
                 check(json.loads(message))
                 print('Valid')
@@ -28,4 +28,4 @@ if __name__ == '__main__':
             message.delete()
 
         print("Moving on")
-        time.sleep(1)
+        time.sleep(10)
